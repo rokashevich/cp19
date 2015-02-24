6635dedc48fe86bb7cb71ccd8122b5def9d624ee
Johan Euphrosine <proppy@google.com>
2015-02-24 14:28:54 -0800

    desktop: fallback download_command for cmake 3.0

--- desktop/External_SDL2.cmake
+++ desktop/External_SDL2.cmake
@@ -3,5 +3,6 @@ set(SDL2_DIR ${CMAKE_BINARY_DIR}/build)
 ExternalProject_Add(SDL2
 	PREFIX ${SDL2_DIR}
 	CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
+	DOWNLOAD_COMMAND ""
 	SOURCE_DIR ../../SDL
 )
