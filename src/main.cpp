bcbcc813975be842e8b94bb720243cc59372a9d4
Aleksandr Rokashevich <rokashevich@gmail.com>
2019-01-16 00:03:51 +0300

    ~ компиляция desktop sdl + gles3/gl3.h

---
+++ src/main.cpp
@@ -0,0 +1,168 @@
+#include "SDL.h"
+#include "SDL_opengl.h"
+#include <time.h>
+#include <GLES3/gl3.h>
+//#include <GL/gl.h>
+//#include <GL/glext.h>
+//#include <glad/glad.h>
+
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 480
+
+const char *vertexShaderSource = "#version 300 es\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char *fragmentShaderSource = "#version 300 es\n"
+        "precision mediump float;\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+void render(){
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    glClearColor(1.f, 0.f, 1.f, 0.f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+int main(int argc, char *argv[])
+{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    int done;
+    SDL_Event event;
+
+    /* initialize SDL */
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Could not initialize SDL\n");
+        return 1;
+    }
+
+    /* seed random number generator */
+    srand(time(NULL));
+
+    /* create window and renderer */
+    window =
+        SDL_CreateWindow(NULL, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT,
+                         SDL_WINDOW_OPENGL);
+    if (!window) {
+        printf("Could not initialize Window\n");
+        return 1;
+    }
+
+
+    SDL_GLContext Context = SDL_GL_CreateContext(window);
+
+
+    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+        glCompileShader(vertexShader);
+        // check for shader compile errors
+        int success;
+        char infoLog[512];
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+            printf("ERROR 1: %s\n", infoLog);
+        }
+        // fragment shader
+            int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+            glCompileShader(fragmentShader);
+            // check for shader compile errors
+            glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+            if (!success)
+            {
+                glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+                printf("ERROR 2: %s\n", infoLog);
+            }
+        // link shaders
+            int shaderProgram = glCreateProgram();
+            glAttachShader(shaderProgram, vertexShader);
+            glAttachShader(shaderProgram, fragmentShader);
+            glLinkProgram(shaderProgram);
+            // check for linking errors
+            glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+            if (!success) {
+                glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+                printf("ERROR 3: %s\n", infoLog);
+            }
+            glDeleteShader(vertexShader);
+            glDeleteShader(fragmentShader);
+
+            // set up vertex data (and buffer(s)) and configure vertex attributes
+                // ------------------------------------------------------------------
+                float vertices[] = {
+                     0.5f,  0.5f, 0.0f,  // top right
+                     0.5f, -0.5f, 0.0f,  // bottom right
+                    -0.5f, -0.5f, 0.0f,  // bottom left
+                    -0.5f,  0.5f, 0.0f   // top left
+                };
+                unsigned int indices[] = {  // note that we start from 0!
+                    0, 1, 3,  // first Triangle
+                    1, 2, 3   // second Triangle
+                };
+                unsigned int VBO, VAO, EBO;
+                glGenVertexArrays(1, &VAO);
+                glGenBuffers(1, &VBO);
+                glGenBuffers(1, &EBO);
+                // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+                glBindVertexArray(VAO);
+
+                glBindBuffer(GL_ARRAY_BUFFER, VBO);
+                glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+                glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+                glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+                glEnableVertexAttribArray(0);
+
+                // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+                glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+                // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+                //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+                // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+                // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+                glBindVertexArray(0);
+
+
+                // uncomment this call to draw in wireframe polygons.
+                glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    /* Enter render loop, waiting for user to quit */
+    done = 0;
+    while (!done) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                done = 1;
+            }
+        }
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+                glClear(GL_COLOR_BUFFER_BIT);
+
+                // draw our first triangle
+                glUseProgram(shaderProgram);
+                glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+                //glDrawArrays(GL_TRIANGLES, 0, 6);
+                glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+                // glBindVertexArray(0); // no need to unbind it every time
+
+
+        SDL_GL_SwapWindow(window);
+        //SDL_Delay(1);
+    }
+
+    /* shutdown SDL */
+    SDL_Quit();
+
+    return 0;
+}
