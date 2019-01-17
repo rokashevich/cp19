1355d73c2284559f0375f914805cc519a3cf03dc
Aleksandr Rokashevich <rokashevich@gmail.com>
2019-01-17 23:45:09 +0300

    Подключилл движение с помощью клавитауры и мыши

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,17 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 480
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
 const char *vertexShaderSource = "#version 300 es\n"
     "layout (location = 0) in vec3 aPos;\n"
-    "uniform mat4 transform;\n"
+    "uniform mat4 model;\n"
+    "uniform mat4 view;\n"
+    "uniform mat4 projection;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = transform * vec4(aPos, 1.0);\n"
+    "   gl_Position = projection * view * model * vec4(aPos, 1.0f);\n"
     "}\0";
 const char *fragmentShaderSource = "#version 300 es\n"
         "precision mediump float;\n"
@@ -24,6 +26,22 @@ const char *fragmentShaderSource = "#version 300 es\n"
     "{\n"
     "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
     "}\n\0";
+
+// camera
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f, 0.0f);
+
+bool firstMouse = true;
+float yaw   = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch =  0.0f;
+float lastX =  800.0f / 2.0;
+float lastY =  600.0 / 2.0;
+float fov   =  45.0f;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
 
 void render(){
     glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -58,7 +76,6 @@ int main(int argc, char *argv[])
 
 
     SDL_GLContext Context = SDL_GL_CreateContext(window);
-
 
     int vertexShader = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -135,43 +152,98 @@ int main(int argc, char *argv[])
                 // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
                 glBindVertexArray(0);
 
-
-                // uncomment this call to draw in wireframe polygons.
-                //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
     /* Enter render loop, waiting for user to quit */
     done = 0;
     while (!done) {
+        float currentFrame = SDL_GetTicks();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 done = 1;
+            } else if( event.type == SDL_KEYDOWN ) {
+              float cameraSpeed = 0.01 * deltaTime;
+              switch( event.key.keysym.sym ) {
+                case SDLK_UP:
+                  cameraPos += cameraSpeed * cameraFront;
+                  break;
+                case SDLK_DOWN:
+                  cameraPos -= cameraSpeed * cameraFront;
+                  break;
+                case SDLK_LEFT:
+                  cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+                  break;
+                case SDLK_RIGHT:
+                  cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+                  break;
+                default:
+                  break;
+                }
+            } else if (event.type == SDL_MOUSEMOTION) {
+              int xpos, ypos;
+              SDL_GetMouseState( &xpos, &ypos );
+              if (firstMouse)
+                  {
+                      lastX = xpos;
+                      lastY = ypos;
+                      firstMouse = false;
+                  }
+
+                  float xoffset = xpos - lastX;
+                  float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+                  lastX = xpos;
+                  lastY = ypos;
+
+                  float sensitivity = 0.1f; // change this value to your liking
+                  xoffset *= sensitivity;
+                  yoffset *= sensitivity;
+
+                  yaw += xoffset;
+                  pitch += yoffset;
+
+                  // make sure that when pitch is out of bounds, screen doesn't get flipped
+                  if (pitch > 89.0f)
+                      pitch = 89.0f;
+                  if (pitch < -89.0f)
+                      pitch = -89.0f;
+
+                  glm::vec3 front;
+                  front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+                  front.y = sin(glm::radians(pitch));
+                  front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+                  cameraFront = glm::normalize(front);
             }
         }
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-                glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-
-        // create transformations
-        glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
-        transform = glm::rotate(transform, (float)SDL_GetTicks(), glm::vec3(0.0f, 0.0f, 1.0f));
-
-        // draw our first triangle
         glUseProgram(shaderProgram);
-        unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        // glBindVertexArray(0); // no need to unbind it every time
 
+        // pass projection matrix to shader (note that in this case it could change every frame)
+        glm::mat4 projection = glm::perspective(glm::radians(fov), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, &projection[0][0]);
+
+        // camera/view transformation
+        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, &view[0][0]);
+
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+
+
+        glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        model = glm::translate(model, glm::vec3( 0.0f,  0.0f,  0.0f));
+        float angle = 30.0f;
+        model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, &model[0][0]);
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         SDL_GL_SwapWindow(window);
-        //SDL_Delay(1);
     }
 
-    /* shutdown SDL */
     SDL_Quit();
 
     return 0;
