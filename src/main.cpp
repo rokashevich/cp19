a34f2e2f0bc8d05ae06f9b5492cf70cf9706859c
Aleksandr Rokashevich <rokashevich@gmail.com>
2019-02-03 16:02:38 +0300

    Геометрический шейдер с матрицей поворота от органов управления

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <EGL/egl.h>
-#include <GLES3/gl3.h>
+#include <GLES3/gl32.h>
+#include <GLES3/gl3ext.h>
 #include "SDL.h"
 //#include "SDL_opengl.h"
 #include <time.h>
@@ -12,20 +13,46 @@
 #define SCREEN_HEIGHT 600
 
 const char *vertexShaderSource = "#version 300 es\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "uniform mat4 model;\n"
-    "uniform mat4 view;\n"
-    "uniform mat4 projection;\n"
+    "layout (location = 0) in vec2 pos;"
+    "layout (location = 1) in vec3 color;"
+    "//uniform mat4 model;"
+    "//uniform mat4 view;\n"
+    "//uniform mat4 projection;\n"
+    "out vec3 vColor;"
     "void main()\n"
-    "{\n"
-    "   gl_Position = projection * view * model * vec4(aPos, 1.0f);\n"
+    "{"
+    "   gl_Position = vec4(pos, 0.0, 1.0f);\n"
+    "   vColor = color;"
     "}\0";
 const char *fragmentShaderSource = "#version 300 es\n"
-        "precision mediump float;\n"
+    "precision mediump float;\n"
+    "in vec3 fColor;"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vec4(fColor, 1.0);"
+    "}\n\0";
+
+const char *geometryShaderSource = "#version 310 es\n"
+    "#extension GL_EXT_geometry_shader : enable\n"
+    "layout(points) in;\n"
+    "layout(triangle_strip, max_vertices = 4) out;\n"
+    "in vec3 vColor[];"
+    "out vec3 fColor;"
+    "uniform mat4 model;"
+    "uniform mat4 view;"
+    "uniform mat4 projection;"
+    "void main(){\n"
+        "fColor = vColor[0];"
+        "gl_Position = projection*view*model*(gl_in[0].gl_Position + vec4(-0.1, -0.1, 0.0, 0.0));"
+        "EmitVertex();\n"
+        "gl_Position = projection*view*model*(gl_in[0].gl_Position + vec4( 0.1, -0.1, 0.0, 0.0));"
+        "EmitVertex();\n"
+        "gl_Position = projection*view*model*(gl_in[0].gl_Position + vec4(-0.1,  0.1, 0.0, 0.0));"
+        "EmitVertex();\n"
+        "gl_Position = projection*view*model*(gl_in[0].gl_Position + vec4( 0.1,  0.1, 0.0, 0.0));"
+        "EmitVertex();\n"
+        "EndPrimitive();\n"
     "}\n\0";
 
 // camera
@@ -50,110 +77,101 @@ void render(){
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     SDL_Window *window;
     SDL_Renderer *renderer;
     int done;
     SDL_Event event;
 
-    /* initialize SDL */
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("Could not initialize SDL\n");
         return 1;
     }
-
-    /* seed random number generator */
     srand(time(NULL));
-
-    /* create window and renderer */
-    window =
-        SDL_CreateWindow(NULL, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT,
-                         SDL_WINDOW_OPENGL);
+    window = SDL_CreateWindow(NULL, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
     if (!window) {
         printf("Could not initialize Window\n");
         return 1;
     }
-
-
     SDL_GLContext Context = SDL_GL_CreateContext(window);
 
+    // vertext shader
     int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-        glCompileShader(vertexShader);
-        // check for shader compile errors
-        int success;
-        char infoLog[512];
-        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-        if (!success)
-        {
-            glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-            printf("ERROR 1: %s\n", infoLog);
-        }
-        // fragment shader
-            int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-            glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-            glCompileShader(fragmentShader);
-            // check for shader compile errors
-            glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-            if (!success)
-            {
-                glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-                printf("ERROR 2: %s\n", infoLog);
-            }
-        // link shaders
-            int shaderProgram = glCreateProgram();
-            glAttachShader(shaderProgram, vertexShader);
-            glAttachShader(shaderProgram, fragmentShader);
-            glLinkProgram(shaderProgram);
-            // check for linking errors
-            glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-            if (!success) {
-                glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-                printf("ERROR 3: %s\n", infoLog);
-            }
-            glDeleteShader(vertexShader);
-            glDeleteShader(fragmentShader);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success){
+      glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+      printf("ERROR: %s\n", infoLog);
+    }
 
-            // set up vertex data (and buffer(s)) and configure vertex attributes
-                // ------------------------------------------------------------------
-                float vertices[] = {
-                     0.5f,  0.5f, 0.0f,  // top right
-                     0.5f, -0.5f, 0.0f,  // bottom right
-                    -0.5f, -0.5f, 0.0f,  // bottom left
-                    -0.5f,  0.5f, 0.0f   // top left
-                };
-                unsigned int indices[] = {  // note that we start from 0!
-                    0, 1, 3,  // first Triangle
-                    1, 2, 3   // second Triangle
-                };
-                unsigned int VBO, VAO, EBO;
-                glGenVertexArrays(1, &VAO);
-                glGenBuffers(1, &VBO);
-                glGenBuffers(1, &EBO);
-                // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-                glBindVertexArray(VAO);
+    // fragment shader
+    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        printf("ERROR: %s\n", infoLog);
+    }
 
-                glBindBuffer(GL_ARRAY_BUFFER, VBO);
-                glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // geometry shader
+    int geometryShader = glCreateShader(GL_GEOMETRY_SHADER);
+    glShaderSource(geometryShader, 1, &geometryShaderSource, nullptr);
+    glCompileShader(geometryShader);
+    glGetShaderiv(geometryShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(geometryShader, 512, NULL, infoLog);
+        printf("ERROR: %s\n", infoLog);
+    }
 
-                glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-                glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // link shaders
+    int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glAttachShader(shaderProgram, geometryShader);
+    glLinkProgram(shaderProgram);
 
-                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-                glEnableVertexAttribArray(0);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+      glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+      printf("ERROR: %s\n", infoLog);
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    glDeleteShader(geometryShader);
 
-                // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-                glBindBuffer(GL_ARRAY_BUFFER, 0);
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
 
-                // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
-                //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    float points[] = {
+        -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+         0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+         0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f,
+    };
 
-                // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-                // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-                glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
 
-    /* Enter render loop, waiting for user to quit */
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "pos");
+    glEnableVertexAttribArray(posAttrib);
+    glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
+
+    GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+    glEnableVertexAttribArray(colAttrib);
+    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE,
+                           6 * sizeof(float), (void*) (3 * sizeof(float)));
+
     done = 0;
     while (!done) {
         float currentFrame = SDL_GetTicks();
@@ -218,7 +236,7 @@ int main(int argc, char *argv[])
             }
         }
 
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
@@ -231,16 +249,16 @@ int main(int argc, char *argv[])
         glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, &view[0][0]);
 
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        //glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 
 
         glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
         model = glm::translate(model, glm::vec3( 0.0f,  0.0f,  0.0f));
-        float angle = 30.0f;
+        float angle = 0.0f;
         model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, &model[0][0]);
 
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawArrays(GL_POINTS, 0, 4);
 
         SDL_GL_SwapWindow(window);
     }
