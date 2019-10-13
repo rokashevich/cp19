5ab270d3c51e638504669ef349bef844b9460800
Aleksandr Rokashevich <rokashevich@gmail.com>
2019-10-13 22:14:50 +0300

    отладка скрипта автосборки

--- android/build-apk.sh
+++ android/build-apk.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 PATH="$ANDROID/ndk:$PATH"                   # for 'ndk-build'
-PATH="$ANDROID/sdk/tools:$PATH"             # for 'android'
-PATH="$ANDROID/sdk/platform-tools:$PATH"    # for 'adb'
-export ANDROID_HOME="$ANDROID/sdk"          # for gradle
+PATH="$ANDROID/tools:$PATH"             # for 'android'
+PATH="$ANDROID/platform-tools:$PATH"    # for 'adb'
+export ANDROID_HOME="$ANDROID"          # for gradle
 export ANDROID_NDK_HOME="$ANDROID/ndk"      # for gradle
 
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
