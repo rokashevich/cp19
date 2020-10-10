e19992417e317290bbf753963952bec18832864f
Aleksandr Rokashevich <rokashevich@gmail.com>
2020-10-10 18:57:43 +0300

    Добавил ещё один тест Vec

--- tests/test_vec.cpp
+++ tests/test_vec.cpp
@@ -7,4 +7,8 @@ TEST(ClassVec, TestSummation) {
   Vec B{{0, 0, 0}, {1, 0, 0}};
   Vec C = A + B;
   EXPECT_EQ(2, C.End().x);
+
+  Vec D{{0, 0, 0}, {-3, 0, 0}};
+  Vec E = C + D;
+  EXPECT_EQ(-1, E.End().x);
 }
