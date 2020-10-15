d17ffb781b4f1341fde1c7c9620ac60fae863ba5
Aleksandr Rokashevich <rokashevich@gmail.com>
2020-10-15 17:21:59 +0300

    Добавил f для float

--- src/helpers/misc.hpp
+++ src/helpers/misc.hpp
@@ -3,6 +3,6 @@
 #include <cmath>
 
 namespace constants {
-const float pi = 3.14;
-const float golden_ratio = (1 + std::sqrt(5)) / 2;
+const float pi = 3.14f;
+const float golden_ratio = (1 + std::sqrt(5.0f)) / 2;
 }  // namespace constants
