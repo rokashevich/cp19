e5ca47e3a678481326fe68576f96c47eac78acbd
Aleksandr Rokashevich <rokashevich@gmail.com>
2023-05-04 16:07:45 +0300

    cppcheck теряет кол-во варнингов...

--- tools/cppcheck.sh
+++ tools/cppcheck.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 cd `dirname $0`/..
-cppcheck --enable=all --inconclusive -v src 2>&1 1>/dev/null |tee >(grep -E ' \[[A-Za-z0-9]+\]$'|wc -l)
+cppcheck --enable=all --inconclusive -v src 2>&1|tee >(grep -E ' \[[A-Za-z0-9]+\]$'|wc -l)
